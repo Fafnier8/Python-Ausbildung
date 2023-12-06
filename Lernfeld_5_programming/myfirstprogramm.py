9da9e88b8329f9cc3183a38e45d2de361d884fcb
Mia Rücker <tobias.ruecker07@gmail.com>
2023-12-06 15:52:53 +0100

    define a little programm that calculates resistance, work and power with error messages for wrong input

--- Lernfeld_5_programming/myfirstprogramm.py
+++ Lernfeld_5_programming/myfirstprogramm.py
@@ -1,18 +1,31 @@
 import math
 
+# Berechnung von Widerstand, Elektrischer Leistung und Elektrischer Arbeit mit Fehlerbehandlung (Aufgabe 8b))
 
 try:
 
     U = float(input("Geben Sie die Spannung in Volt ein: \n"))
     I = float(input("Geben Sie die Stromstärke in Ampere ein: \n"))
+    t = float(input("Geben Sie die Zeit in Sekunden an in der die Arbeit geleistet wird: \n"))
 
     R = U / I
+
+    P = U * I
+
+    W = P * t
 
 
     print("Der elektrische Widerstand beträgt: {0:.2f} Ohm".format(R))
 
+    print("Die elektrische Leistung beträgt: {0:.2f} Watt".format(P))
+
+    print("Die elektrische Arbeit, die in {0} s geleistet wurde, beträgt: {1:.2f} Ws".format(t, W))
+
 
 except Exception as e:
     print("Es ist der folgende Fehler aufgetreten: \n" + e.args[0])
+
+
+#  Berechnung der Zeit in Minuten bei Eingabe von Datenmenge in MB und Datenübertragungsrate in kbit/s 
 
 
